a0a484fb693248892aaeae5ce372867130c87603
Murad460 <126158669+Murad460@users.noreply.github.com>
2024-09-16 09:33:36 +0530

    Kaggle Notebook | oops1 | Version 1

---
+++ oops1.py
@@ -0,0 +1,16 @@
+# %% [code]
+class plant_disease_detect:
+    def __init__(self,plant_image):
+        self.plant_image()
+    def plant_image(self):
+        return plant.feature()
+    def plant_shape(self):
+        p=self.plant_image()
+        if p.feature>self.plant_image()
+            return plant.detect_outline()
+        else:
+            return None
+    def plant_type(self):
+        model=model.predict(self.plant_shape())
+        return model
+        
